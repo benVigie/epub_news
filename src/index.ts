47f125143f4413901442077300c6b67b8ac62a0f
Benjamin Vigie <vigie.benjamin@outlook.com>
2024-09-16 16:03:28 -0400

    Avoid having duplicated articles when fetching from the same source

--- src/index.ts
+++ src/index.ts
@@ -70,13 +70,18 @@ async function main() {
   // Now create a media source for each RSS feed, fetch & format them and add them to the ebook list
   let epubContent: EpubContentOptions[] = [];
   let customCss: string = "";
+  let parsed: string[] = [];
   let epubCover: string | undefined;
   for (const rssFeed of rssFeeds) {
     // Create the MediaSource which can handle this feed
     const mediaSource = createMediaSource(rssFeed, prg_options.debug);
     if (mediaSource) {
-      // Retrieve news from service
+      // Retrieve news from service and remove already parsed ones
       let newsList = await mediaSource.retrieveNewsListFromSource();
+      newsList = newsList.filter((news) => (news.guid ? !parsed.includes(news.guid) : true));
+
+      // Add news guid to the parsed articles to not treat several times the same ones
+      parsed = parsed.concat(newsList.map((news) => (news.guid ? news.guid : "")));
 
       // If we're in interactive mode, allow the user to filter the articles he wants to keep
       if (prg_options.interactive) {
